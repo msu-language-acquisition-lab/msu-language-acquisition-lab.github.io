aaa5f5b9fe8a5753d39c76d1d28113eabc00f3da
Alan Munn <amunn@gmx.com>
2024-09-01 13:19:43 -0400

    Delete .github/makelabbib.sh

--- .github/makelabbib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pandoc lab-publications.bib -s -f biblatex -t markdown -o lab-pubs.txt
-pandoc --citeproc --csl apa-cv lab-pubs.txt > tmp-bib.txt
-cat bibliography-header.txt tmp-bib.txt > lab-publications.md
