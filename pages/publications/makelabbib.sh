5fb3cbe3d37db04f22e5d186cd7318035dee62d1
Alan Munn <amunn@msu.edu>
2024-09-04 09:43:38 -0400

    Updated bib

--- pages/publications/makelabbib.sh
+++ pages/publications/makelabbib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pandoc lab-publications.bib -s -f biblatex -t markdown -o lab-pubs.txt
-pandoc --citeproc --csl apa-cv lab-pubs.txt > tmp-bib.txt
-cat bibliography-header.txt tmp-bib.txt > lab-publications.md
+pandoc --citeproc --csl apa-cv lab-pubs.txt -o tmp-bib.html
+cat bibliography-header.txt tmp-bib.html > lab-publications.md
